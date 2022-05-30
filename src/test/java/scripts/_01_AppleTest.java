b00ae957460e8aa79ae572bbccf59056df086d3e
TorrieG <102880414+TorrieG@users.noreply.github.com>
2022-05-30 11:52:27 -0700

    added smoke, regression, dropdowns

--- src/test/java/scripts/_01_AppleTest.java
+++ src/test/java/scripts/_01_AppleTest.java
@@ -1,45 +1,29 @@
 package scripts;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
-import utilities.Driver;
 
+public class _01_AppleTest extends Base {
 
-
-public class _01_AppleTest {
-
-
-    //Declare your driver
-    WebDriver driver;
-
-    // 1. Initial set up - before each test
-    @BeforeMethod
-    public void setup(){
-        driver = Driver.getDriver();
-    }
-
-    // 2. Validation of test - test
-    @Test
-    public void testAppleTitle(){
+    @Test(priority = 1, description = "TC345")
+    public void testAppleTitle() {
         driver.get("https://www.apple.com/");
 
         String expectedTitle = "Apple"; // requirement
         String actualTitle = driver.getTitle(); // development
 
-    /*
-    Instead of these if-else validation, we will use TestNG assertion
-    if(actualTitle.equals(expectedTitle)) System.out.println("Apple title validation passed");
-    else System.out.println("Apple title validation failed");
-     */
+        /*
+        Instead of these if-else validation, we will use TestNG assertion
+        if(actualTitle.equals(expectedTitle)) System.out.println("Apple title validation passed");
+        else System.out.println("Apple title validation failed");
+         */
 
         Assert.assertEquals(actualTitle, expectedTitle);
     }
 
-    @Test
-    public void testAppleURL(){
+    @Test(priority = 2, description = "TC873")
+    public void testAppleURL() {
         driver.get("https://www.apple.com/");
 
         String expectedURL = "https://www.apple.com/";
@@ -48,17 +32,34 @@ public class _01_AppleTest {
         Assert.assertEquals(actualURL, expectedURL);
     }
 
-    @Test
-    public void x(){
+    @Ignore
+    @Test(priority = 3, description = "TC092")
+    public void X() {
         //fail() method is used to fail a test on purpose
-        //used for known issues
-        Assert.fail("Failure on purpose - bug ticket created KJKJ798798");
+        //We use this for known issues
+        Assert.fail("Failure on purpose");
     }
 
-    // 3. Teardown actions - after each test
-    @AfterMethod
-    public void teardown(){
+
+
+
+    /*
+    public static void main(String[] args) {
+        // 1. Initial set up - before each test
+        WebDriver driver = Driver.getDriver();
+
+        // 2. Validation of test - test
+        driver.get("https://www.apple.com/");
+
+        String expectedTitle = "Apple"; // requirement
+        String actualTitle = driver.getTitle(); // development
+
+        if(actualTitle.equals(expectedTitle)) System.out.println("Apple title validation passed");
+        else System.out.println("Apple title validation failed");
+
+        // 3. Teardown actions - after each test
         Driver.quitDriver();
     }
 
+     */
 }
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,15 +1,17 @@
 package utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.NotFoundException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
-
     // 1. Private Constructor
-    private Driver() {
+    private Driver(){
 
     }
 
@@ -17,27 +19,43 @@ public class Driver {
     private static WebDriver driver;
 
     // 3. Get Driver and quit driver Method
-    public static WebDriver getDriver() {
-        if (driver == null) {
+    public static WebDriver getDriver(){
+        if(driver == null){
             // Telling your system where your chrome driver is located
-            WebDriverManager.chromedriver().setup();
+            //System.setProperty("webdriver.chrome.driver", "/Users/techglobal/IdeaProjects/selenium_intro/chromedriver");
 
-            // Create the object of the web browser that you are automating
-            driver = new ChromeDriver();
+            String browser = "chrome"; // define which browser you will run your test in
+
+            switch (browser){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driver = new SafariDriver();
+                    break;
+                default:
+                    throw new NotFoundException("Browser IS NOT DEFINED properly!!!");
+            }
             driver.manage().window().maximize();
-            //waiting only that web element to be EXISTED
             driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         }
         return driver;
     }
 
-    public static void quitDriver() {
-//        try {
+    public static void quitDriver(){
+//        try{
 //            Thread.sleep(3000);
-//        } catch (Exception e) {
+//        }
+//        catch (Exception e){
 //            e.printStackTrace();
 //        }
-        if (driver != null) {
+        if(driver != null){
             driver.manage().deleteAllCookies();
             driver.quit();
             driver = null;
